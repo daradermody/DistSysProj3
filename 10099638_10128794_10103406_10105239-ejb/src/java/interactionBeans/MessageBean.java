a05c5fe51daf81712d7f07243c7986dd214ee32e
daradermody <daradermody@gmail.com>
2014-04-28 00:11:26 +0100

    Commented out MessageBean

--- 10099638_10128794_10103406_10105239-ejb/src/java/interactionBeans/MessageBean.java
+++ 10099638_10128794_10103406_10105239-ejb/src/java/interactionBeans/MessageBean.java
@@ -1,32 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package interactionBeans;
-
-import javax.ejb.ActivationConfigProperty;
-import javax.ejb.MessageDriven;
-import javax.jms.Message;
-import javax.jms.MessageListener;
-
-/**
- *
- * @author root
- */
-@MessageDriven(activationConfig = {
-    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "java:global/10099638_10128794_10103406_10105239/10099638_10128794_10103406_10105239-ejb/interactCustomer!interactionBeans.interactCustomerLocal"),
-    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
-})
-public class MessageBean implements MessageListener {
-    
-    public MessageBean() {
-    }
-    
-    @Override
-    public void onMessage(Message message) {
-        System.out.println("MESSAGE RECEIVED!!");
-    }
-    
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//
+//package interactionBeans;
+//
+//import javax.ejb.ActivationConfigProperty;
+//import javax.ejb.MessageDriven;
+//import javax.jms.Message;
+//import javax.jms.MessageListener;
+//
+///**
+// *
+// * @author root
+// */
+//@MessageDriven(activationConfig = {
+//    @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "java:global/10099638_10128794_10103406_10105239/10099638_10128794_10103406_10105239-ejb/interactCustomer!interactionBeans.interactCustomerLocal"),
+//    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+//})
+//public class MessageBean implements MessageListener {
+//    
+//    public MessageBean() {
+//    }
+//    
+//    @Override
+//    public void onMessage(Message message) {
+//        System.out.println("MESSAGE RECEIVED!!");
+//    }
+//    
+//}
